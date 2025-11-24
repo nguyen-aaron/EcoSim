57a68e8536ddcd3c761a7847e8d4e85455cff4ba
nguyen-aaron <aaronnguyen@MacBook-Air-4.local>
2025-11-23 20:15:55 -0800

    updated files for Render compatibility

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
+import os
 from mesa_abm.server import server
 
+# Render assigns a dynamic port through the PORT environment variable
+port = int(os.environ.get("PORT", 8522))
+
+server.port = port
 server.launch()
